a58660225853b6c41a026733602b37fd949cb351
Santiago Floricich Elejalde <S.FloricichElejalde@albynschool.org>
2025-03-10 09:20:57 +0000

    help

--- login.py
+++ login.py
@@ -1,6 +1,10 @@
-username = input("Please enter your username")
-password = input("Please enter your password")
-if username == "Santi" and password == "themanwhoknocks":
-    print("login accepted")
-else:
-    print("login rejected")
+
+accepted = bool(False)
+while accepted == False:
+    username = input("Please enter your username")
+    password = input("Please enter your password")
+    if username == "Santi" and password == "themanwhoknocks":
+        print("login accepted")
+        accepted = bool(True)
+    else:
+        print("login rejected")
