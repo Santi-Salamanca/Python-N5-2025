8c39a018814c8a36f3c85e7c24749dbe3809f12a
Santiago Floricich Elejalde <S.FloricichElejalde@albynschool.org>
2025-03-10 09:15:54 +0000

    hello

---
+++ login.py
@@ -0,0 +1,6 @@
+username = input("Please enter your username")
+password = input("Please enter your password")
+if username == "Santi" and password == "themanwhoknocks":
+    print("login accepted")
+else:
+    print("login rejected")
