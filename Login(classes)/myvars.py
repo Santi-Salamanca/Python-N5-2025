a3cf52fcd380e6fde97d75ae2fcbea681edd16e0
Santiago Floricich Elejalde <S.FloricichElejalde@albynschool.org>
2025-04-16 15:29:38 +0100

    asd

--- Login(classes)/myvars.py
+++ Login(classes)/myvars.py
@@ -3,10 +3,14 @@ class user:
         self.username = username
         self.password = password
         self.notes = notes
-        notes = [[]]
 
 #example user
-person1 = user("John", "John123",["chores",["sweep house", "clean room", "take dog for a walk"]] )
+person1 = user("John", "John123",["chores",["sweep house", "clean room", "take dog for a walk"]
+                                  ,"school",["math homework", "science project"]
+                                  ,"work",["finish report", "send email"]])
 print(person1.username)
 print(person1.password)
-print(person1.notes[0][1])
+print(person1.notes[1][0])
+print(person1.notes[1][1])
+print(person1.notes[3][0])
+print(person1.notes[5][1]) # the chores school and work count as a number for index and are seperate
