fbad4a0e57a14e65c45a1ab98c05e44993f27352
Santiago Floricich Elejalde <S.FloricichElejalde@albynschool.org>
2026-02-02 11:46:10 +0000

    asdsad

--- 2025_assignment_15makrs.py
+++ 2025_assignment_15makrs.py
@@ -1,4 +1,4 @@
-#modules
+#MODULES
 import random
 
 #initialise variables
@@ -12,13 +12,13 @@ fruitsSelection = []
 #get and store valid fruit selections
 fruit = str(input("Please enter your fruit: "))
 fruitsSelection.append(fruit)
-#add 1 to counter
-counter += 1
+counter = counter + 1
+
 
 #get and store decision regarding entering aother fruit
 decision = str(input("Do you want to enter another fruit? (yes/no): "))
 
-#decision = yes and counter <  6
+#add 1 to counter
 while decision == "yes" and counter < 6:
 
     #repeat previous
@@ -28,16 +28,17 @@ while decision == "yes" and counter < 6:
     decision = str(input("DO you want to enter another fruit? (yes/no): "))
 
 
+#de
 ############
-randomNum = int(random.randint(0,9))
+number = int(random.randint(0,9))
 
 print("The fruits you entered were.")
 #######display all users entered fruit names
 for _ in fruitsSelection:
     print(_)
 
-mysteryFruitChosen = str(mysteryFruits[randomNum])
-print("The myster fruit is: " + mysteryFruitChosen)
+mysteryFruitChosen = str(mysteryFruits[number])
+print("The mystery fruit is: " + mysteryFruitChosen)
 
 ###################
 ####LAST PART
@@ -46,8 +47,8 @@ print("The myster fruit is: " + mysteryFruitChosen)
 counter +=  1    
 
 if counter < 3:
-    print("We reccomend a milkshake")
+    print("We recommend a milkshake")
 elif counter == 3 or counter == 4:
-    print("We reccomend a smoothie")
+    print("We recommend a smoothie")
 else:
-    print("We reccomend a fruit juice")
+    print("We recommend a fruit juice")
